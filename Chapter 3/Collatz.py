cb06c9eae7a0f71254facf1ff898623460bf9c85
ConstanceIl <64924105+ConstanceIl@users.noreply.github.com>
2020-11-25 18:48:28 -0300

    Update Collatz.py

--- Chapter 3/Collatz.py
+++ Chapter 3/Collatz.py
@@ -1,4 +1,6 @@
+# Define the function
 def collatz(number):
+  # If the number is even, perform float division. If the number is odd, multiply it 3 times and add 1
   if number % 2 == 0:
     result = number // 2
     print(result)
@@ -8,7 +10,7 @@ def collatz(number):
     print(result)
     return result
 
-# Passive-aggresive function
+# Passive-aggresive function. Ask for user input
 def f():
   inp = input('Please type a positive integer value: ')
   try:
